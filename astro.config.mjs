e3c4028bb8078859d1eb5e89f2368f97198fef97
louwei <402791753@qq.com>
2025-06-13 10:05:32 +0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	base: 'https://jackwei.github.io/jackblog'
+	base: 'https://jackwei.github.io'
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
